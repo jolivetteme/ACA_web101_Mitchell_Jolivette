e804a956eff93e60d849d5d1062905604bd85a46
Mitchell Jolivette <jolivetteme@gmail.com>
2019-02-25 20:34:11 -0600

    Finished front-end

---
+++ ticTacToe/scripts/ticTacToe.js
@@ -0,0 +1,10 @@
+var gameMarker = "x";
+
+function changeMarkerToX(){
+  console.log("The x button was clicked!");
+  var gameMarker=x;
+}
+function changeMarkerToO(){
+  console.log("The o button was clicked!");
+  var gameMarker=O;
+}
