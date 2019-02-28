d668b03f5c6d99ec7f558d03107392bd1926f499
Mitchell Jolivette <jolivetteme@gmail.com>
2019-02-27 18:18:04 -0600

    Finished tic-tac-tow

--- ticTacToe/scripts/ticTacToe.js
+++ ticTacToe/scripts/ticTacToe.js
@@ -1,10 +1,64 @@
 var gameMarker = "x";
+var myButtons;
 
-function changeMarkerToX(){
-  console.log("The x button was clicked!");
-  var gameMarker=x;
+function changeMarkerToX()
+  {
+    placeMark('home');
+    console.log("The x button was clicked!");
+    // return true;
 }
-function changeMarkerToO(){
-  console.log("The o button was clicked!");
-  var gameMarker=O;
-}
+
+function changeMarkerToO()
+  {
+
+    placeMark('home');
+    console.log("The O button was clicked!");
+    // return true;
+  }
+
+function placeMark(id)
+  {
+    switch (id) {
+      case "home":
+        //code for center
+        document.getElementById("home").innerHTML = gameMarker;
+        break;
+      case "up":
+      //code
+        document.getElementById("down").innerHTML = gameMarker;
+        break;
+      case "down":
+      //code
+        document.getElementById("left").innerHTML = gameMarker;
+        break;
+      case "left":
+      //code
+        document.getElementById("right").innerHTML = gameMarker;
+        break;
+      case "right":
+      //code
+        document.getElementById("top-left").innerHTML = gameMarker;
+        break;
+      case "top-left":
+      //code
+        document.getElementById("top-right").innerHTML = gameMarker;
+        break;
+      case "top-right":
+      //code
+        document.getElementById("bottom-left").innerHTML = gameMarker;
+        break;
+      case "bottom-left":
+      //code
+        document.getElementById("bottom-right").innerHTML = gameMarker;
+        break;
+      case "bottom-right":
+      //code
+        document.getElementById()
+        break;
+
+      default:
+      alert("Something went wrong");
+    }
+    // let theButton = document.getElementById(id);
+    // document.getElementById(id).innerHTML=gameMarker;
+  }
