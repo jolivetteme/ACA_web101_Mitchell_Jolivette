03ab253be85bbebe3130280ac6db5410b7cd5157
Mitchell Jolivette <jolivetteme@gmail.com>
2019-03-08 10:25:20 -0600

    Finished hair_LandingPage

--- hair_LangingPage/hair.js
+++ hair_LangingPage/hair.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-  $('#top-nav ul').toggle();
+  $('#top-nav ul').show();
   $('nav#top-nav div.menu-control').on('click',function(){
     $('nav#top-nav ul').toggle();
   });
